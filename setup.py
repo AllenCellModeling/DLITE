0823b7f493c78c0e40ac5258550694ede05cc9d6
Ritvik_Vasan <ritvikv@alleninstitute.org>
2019-02-25 12:19:36 -0800

    Setup and requirements
    Added
    setup.py, requirements.txt, CHANGES.txt, MANIFEST.in, README.txt
    __init__.py in cell_soap

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+import sys
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
+setup(name='DLITE',
+      description=' a dynamic force-inference model to estimate tensions in colony time-series',
+      author='Vasan, Ritvik and Maleckar, Mary M. and Williams, C. David. and Rangamani, Padmini',
+      author_email='cdave@alleninstitute.org',
+      url='https://github.com/AllenCellModeling/DLITE',
+      packages=['cell_soap'],
+      license='LICENSE.txt',
+      version='0.1.0',
+      keywords=['Force-inference', 'tension', 'dynamic' ],
+      long_description=open('README.txt').read(),
+      install_requires=required)
