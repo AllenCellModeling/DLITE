35d3f4c6792ab83132895f0af0ab926e73f23241
Dave Williams <cdw@users.noreply.github.com>
2019-06-19 16:22:20 -0700

    Moving init info from cookie cutter to local

--- DLITE/__init__.py
+++ DLITE/__init__.py
@@ -1,3 +1,13 @@
+"""Top-level package for DLITE."""
+
+__author__ = 'Ritvik Vasan'
+__email__ = 'rvasan@eng.ucsd.edu'
+__version__ = '0.1.0'
+
+
+def get_module_version():
+    return __version__
+
 import matplotlib as mpl
 mpl.use('TkAgg')
 
