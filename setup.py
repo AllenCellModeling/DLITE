c18413b8b06a4401902706f5f14f711d5b4563aa
Unknown <ritvikv@alleninstitute.org>
2019-08-23 10:11:31 -0700

    remove history

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 test_requirements = [
     'codecov',
     'flake8',
